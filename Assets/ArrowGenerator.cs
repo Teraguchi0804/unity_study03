5bd2cb2e61b9f20bacf7c9b396e60017c7ec9aa4
Norikazu Teraguchi <norimaki.tera@gmail.com>
2017-03-14 22:23:18 +0900

    ArrowGeneratorスクリプトをPrefabに渡す

--- Assets/ArrowGenerator.cs
+++ Assets/ArrowGenerator.cs
@@ -3,22 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.VR.WSA.WebCam;
 
-public class ArrowGenerator : MonoBehaviour
-{
-    private GameObject arrowPrefab;
-    private float span = 1.0f;
-    private float delta = 0;
+public class ArrowGenerator : MonoBehaviour {
 
-	// Update is called once per frame
-	void Update ()
-	{
-	    this.delta += Time.deltaTime;
-	    if (this.delta > this.span)
-	    {
-	        this.delta = 0;
-	        GameObject go = Instantiate(arrowPrefab) as GameObject;
-	        int px = Random(-6, 7);
-	        go.transform.position = new Vector3(px, 7, 0);
-	    }
+	public GameObject arrowPrefab;
+	float span = 0.5f;
+	float delta = 0;
+
+	void Update() {
+		this.delta += Time.deltaTime;
+		if(this.delta > this.span) {
+			this.delta = 0;
+			GameObject go = Instantiate(arrowPrefab) as GameObject;
+			int px = Random.Range(-6, 7);
+			go.transform.position = new Vector3(px, 7, 0);
+		}
 	}
 }
